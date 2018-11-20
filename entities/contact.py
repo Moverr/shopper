e5caf7c6448338ffa308d5a14be268407691c81a
Muyinda Rogers <moverr@gmail.com>
2018-11-20 14:38:02 +0300

    Modular init and Contacts.py :

---
+++ entities/contact.py
@@ -0,0 +1,29 @@
+class Contact(object):
+    def __init__(self):
+        pass
+ 
+    def setType(self,contacttype):
+        self.contacttype = contacttype
+    
+    def getType(self):
+        return  self.contacttype
+
+    def setDetails(self,details):
+        self.details = details
+    
+    def getDetails(self):
+        return  self.details
+
+    def setStatus(self,status):
+        self.status = status
+    
+    def getStatus(self):
+        return  self.status
+
+     
+
+
+
+    
+    
+        
