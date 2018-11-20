94a1f5347f786e2bb4e261fd73b8310e1165be49
Muyinda Rogers <moverr@gmail.com>
2018-11-20 13:36:10 +0300

    Debugging :

---
+++ entities/category.py
@@ -0,0 +1,29 @@
+class Category(object):
+    def __init__(self):
+        pass
+ 
+    def setName(self,name):
+        self.name = name
+    
+    def getName(self):
+        return  self.name
+
+    def setCode(self,code):
+        self.code = code
+    
+    def getCode(self):
+        return  self.code
+
+    def setStatus(self,status):
+        self.status = status
+    
+    def getStatus(self):
+        return  self.status
+
+     
+
+
+
+    
+    
+        
