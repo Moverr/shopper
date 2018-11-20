87542b6ad450413c957bade1dd7770434a6e545d
Muyinda Rogers <moverr@gmail.com>
2018-11-20 13:43:54 +0300

    Tags Entity :

---
+++ entities/tag.py
@@ -0,0 +1,30 @@
+class Tag(object):
+    def __init__(self):
+        pass
+ 
+
+    def setName(self,name):
+        self.name = name
+    
+    def getName(self):
+        return  self.name
+
+    def setCode(self,code):
+        self.code = code
+    
+    def getCode(self):
+        return  self.code
+
+    def setStatus(self,status):
+        self.status = status
+    
+    def getStatus(self):
+        return  self.status
+
+     
+
+
+
+    
+    
+        
