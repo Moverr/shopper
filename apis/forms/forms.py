7ce0154dd4c6e1ede0b5232fccc0e75884484f34
Muyinda Rogers <moverr@gmail.com>
2018-11-23 11:15:18 +0300

    Debugging :

--- apis/forms/forms.py
+++ apis/forms/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,Email,EqualTo
 
 class RegistrationForm(FlaskForm):
@@ -12,3 +12,14 @@ class RegistrationForm(FlaskForm):
                          validators=[DataRequired(),Length(min=6,max=20)])
     confirm_password =  PasswordField('Confirm Password',
                          validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email    = StringField('Email',
+                        validators=[DataRequired(),Email()])
+    # Password regular expression making sure that you add also characters and integers : 
+    password =  PasswordField('Password',
+                         validators=[DataRequired(),Length(min=6,max=20)])
+    remember =  BooleanField('Remember Me')       
+    submit = SubmitField('Sign In')
