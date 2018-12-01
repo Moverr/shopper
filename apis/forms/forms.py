bb106f219ddbc99ede6052b8b7092eb9afea398f
Muyinda Rogers <moverr@gmail.com>
2018-12-01 14:06:01 +0300

    Form Registration Form Handling etc :

--- apis/forms/forms.py
+++ apis/forms/forms.py
@@ -3,8 +3,7 @@ from wtforms import StringField, PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,Email,EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                        validators=[DataRequired(),Length(min=2,max=20)])
+    
     email    = StringField('Email',
                         validators=[DataRequired(),Email()])
     # Password regular expression making sure that you add also characters and integers : 
